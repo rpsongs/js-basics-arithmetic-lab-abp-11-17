aa98705d550b5aadc7985b6cf8c59f930d5235de
Rachel Pearse <rpsongs33@gmail.com>
2017-11-15 04:12:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ var ageIsValid =  Number.isInteger(currentAge);
 
 var randomNumber = Math.floor(Math.random(21));
 var randomInteger = Math.floor(randomNumber);
-var randomUserID = Math.ceil(Math.random(randomInteger));
+var randomUserID = Math.ceil(randomInteger);
